b4423e90d1975281d84196715c792a5ed4e3e73c
Andey Drugov <dr_ugoff@mail.ru>
2022-11-22 13:28:45 +0700

    changed Example 011 Array Library

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,15 +1,17 @@
-﻿void FillArray(int[] collection)
+﻿// voide означает, что функция (метод) ничего не возвращает
+
+void FillArray(int[] collection) // Функция (метод) FillArray заполнения массива
 {
     int length = collection.Length;
     int index = 0;
     while (index < length)
     {
-        collection[index] = new Random().Next(1, 10);
+        collection[index] = new Random().Next(1, 10); // Генерация рандомных значений каждого элемента массива collection
         index++;
     }
 }
 
-void PrintArray(int[] col)
+void PrintArray(int[] col) // Функция (метод) печати массива в консоль
 {
     int count = col.Length;
     int position = 0;
@@ -20,29 +22,29 @@ void PrintArray(int[] col)
     }
 }
 
-int IndexOf(int[] collection, int find)
-{
-    int count = collection.Length;
-    int index = 0;
-    int position = -1;
-    while (index < count)
-    {
-        if (collection[index] == find)
-        {
-            position = index;
-            break;
-        }
-        index++;
-    }
-    return position;
-}
+// int IndexOf(int[] collection, int find)
+// {
+//     int count = collection.Length;
+//     int index = 0;
+//     int position = -1;
+//     while (index < count)
+//     {
+//         if (collection[index] == find)
+//         {
+//             position = index;
+//             break;
+//         }
+//         index++;
+//     }
+//     return position;
+// }
 
-int[] array = new int[10];
+int[] array = new int[10]; //Создали новый массив array, в котором 10 элеменов (по умолчанию он наполнен нулями)
 
 FillArray(array);
 PrintArray(array);
 
-Console.WriteLine();
+// Console.WriteLine();
 
-int pos = IndexOf(array, 4);
-Console.WriteLine(pos);
+// int pos = IndexOf(array, 4);
+// Console.WriteLine(pos);
