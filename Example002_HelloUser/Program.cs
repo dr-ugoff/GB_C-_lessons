5e00f6eecc59d19461b6f904b9e9c5c7c972f33d
Andey Drugov <dr_ugoff@mail.ru>
2022-11-22 07:07:07 +0700

    add Example 010 Method Array project

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write ("Введите ваше имя: ");
-string username = Console.ReadLine();
+var username = Console.ReadLine();
 Console.Write ("Привет, ");
 Console.Write (username);
 Console.Write ("!");
